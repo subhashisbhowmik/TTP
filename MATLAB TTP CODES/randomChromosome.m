f11227799df11145ba6df34ae6d238c8162ec837
Subhashis Bhowmik <subhashis.b96@gmail.com>
2016-08-26 02:30:50 +0530

    Start From any city- FIXED

--- MATLAB TTP CODES/randomChromosome.m
+++ MATLAB TTP CODES/randomChromosome.m
@@ -1,6 +1,7 @@
 function ch=randomChromosome(TTPSetObject,maxObjects)
     noCities=length(TTPSetObject.distanceMatrix);
-    cityArray=[1 randperm(noCities-1)+1];
+    %cityArray=[1 randperm(noCities-1)+1];
+    cityArray=[randperm(noCities)];
     objectList=reshape(rand(maxObjects*noCities,1)*rand()>0.5,noCities,maxObjects);
     ch=Chromosome(cityArray,objectList,TTPSetObject);
 end
